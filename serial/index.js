8ff4d2b84d200941eef19e8e28e2bd52b4ad10bf
Rachel <rachel.ulgado@gmail.com>
2018-10-13 12:18:12 -0700

    updating to remove static references

--- serial/index.js
+++ serial/index.js
@@ -1,27 +1,20 @@
 const SerialPort = require("serialport");
-const express = require('express');
-const app = express();
-const server = app.listen(3001);
-app.use(express.static('public'));
-
-const io = require('socket.io')(server);
+const express    = require('express');
+const app        = express();
+const server     = app.listen(3001);
+const io         = require('socket.io')(server);
 
 const port = new SerialPort('/dev/tty.usbmodem141101', {
   baudRate: 9600
 });
 
-//Serve index.html when some make a request of the server
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
 port.on('open', function() {
-    console.log('Port open');
+  console.log('Port open');
 });
 
 port.on('data', function(data) {
-    // console.log(data.toJSON());
-    // console.log(data);
-    console.log(data.toString('utf8'));
-    io.sockets.emit('mysocket', data);
+  // console.log(data);
+  // console.log(data.toJSON());
+  console.log(data.toString('utf8'));
+  io.sockets.emit('mysocket', data);
 });
